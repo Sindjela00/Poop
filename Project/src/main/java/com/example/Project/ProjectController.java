a3d77f195439f2896b1a9a93f525148fccaedbe1
DarkScaryDoor <aco.sindjelic@gmail.com>
2021-08-14 15:50:27 +0200

    Update ProjectController.java

--- Project/src/main/java/com/example/Project/ProjectController.java
+++ Project/src/main/java/com/example/Project/ProjectController.java
@@ -44,8 +44,6 @@ public class ProjectController {
     @GetMapping("/potrazi")
     public @ResponseBody List<Oglas> potrazi(@RequestParam(required = false) String poslodavac,@RequestParam(required = false) String mesto,@RequestParam(required = false) String radno_vreme){
         //db.Daj_Oglase(poslodavac, Mesto, "1", radno_vreme);
-
-
         return null;
     }
     @GetMapping("/profile")
